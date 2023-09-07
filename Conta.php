02cdebe67a28e533863cea3b021cf1409bcb2242
dvctrl <davidmouzinho.contato@gmail.com>
2023-09-06 21:19:08 -0300

    construtor e destrutor

--- Conta.php
+++ Conta.php
@@ -29,4 +29,26 @@
         }
 
 
+        function __construct($agencia,$codigo,$dataDecriacao,$titulo,$senha,$saldo,$cancelada)
+        {
+
+            $this->agencia= $agencia;
+            $this->codigo = $codigo;
+            $this->dataDecriacao =$dataDecriacao;
+            $this->titulo =$titulo;
+            $this->senha = $senha;
+
+            //chamada a outro método da classe
+            $this->depositar($saldo);
+            $this->cancelada = false;
+        }
+         // metodo destrutor finaliza o projeto
+
+          function __destruct(){
+            echo "<br>Objeto{$this->nome} finalizado ... <br>";
+          }
+
+
+
+
     }
--- Pessoa.php
+++ Pessoa.php
@@ -27,4 +27,19 @@
             } 
         }
 
+        function __construct($codigo,$nome,$altura,$idade,$nascimento,$escolaridade,$salario){
+            $this->codigo= $codigo;
+            $this->nome = $nome;
+            $this->altura =$altura;
+            $this->idade =$idade;
+            $this->nascimento = $nascimento;
+            $this->escolaridade = $escolaridade;
+            $this->salario = $salario;
+        }
+         // metodo destrutor finaliza o projeto
+
+          function __destruct(){
+            echo "<br>Objeto{$this->nome} finalizado ... <br>";
+          }
+
     }
